a13518845a97f53b79a6438be16d27cb6c08c801
mikai <dreamfever2017@yahoo.com>
2026-05-17 19:07:58 +0800

    fix(logging): add slf4j runtime binding

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
 
     implementation(compose.desktop.currentOs)
     implementation(libs.kotlinx.coroutinesSwing)
+    runtimeOnly(libs.logback.classic)
 
     implementation(libs.compose.uiToolingPreview)
 }
